11420e385c9b2a888c1ba923290a4442edf0fe02
Tijan <tijanmdr@gmail.com>
2016-11-17 15:51:45 +0545

    Transclude: true

--- css-js/app.js
+++ css-js/app.js
@@ -23,6 +23,7 @@ newApp.directive('randomFruit', [function() {
 			title: "="
 		},
 		templateUrl: 'views/random.html',
+		transclude: true,
 		controller: function ($scope) {
 			$scope.random = Math.floor(Math.random() * 4);
 		}
